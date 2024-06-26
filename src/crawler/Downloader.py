18cd3e3e2649fc64d9dd78647d8c49d31da2b232
Maintainer <thomse26@gmail.com>
2024-06-26 18:09:03 +0200

    improved image gatherer performance by checking img-alt tag

--- src/crawler/Downloader.py
+++ src/crawler/Downloader.py
@@ -3,12 +3,29 @@ from io import BytesIO
 import requests
 from PIL import Image
 from selenium import webdriver
+from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
+class presence_of_element_with_img_alt:
+    def __init__(self, locator, alt_value):
+        self.locator = locator
+        self.alt_value = alt_value
+
+    def __call__(self, driver):
+        try:
+            element = driver.find_element(*self.locator)
+            if element.get_attribute('alt') == self.alt_value:
+                return element
+            else:
+                print(element.get_attribute('alt')," == ", self.alt_value )
+                return False
+        except NoSuchElementException:
+            print("No alt tag")
+            return False
 
 class ImageDownloader:
 
@@ -23,43 +40,14 @@ class ImageDownloader:
 
     def download_image_buondua(self, url: str, savingPath_with_file: str):
         self.driver.get(url)
-        try:
-            WebDriverWait(self.driver, 10).until_not(
-                EC.presence_of_element_located((By.CLASS_NAME, 'challenge-running')))
-        except:
-            self.driver.reload()
-        try:
-            image_page = WebDriverWait(self.driver, 5).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'shrinkToFit')))
-            image_url = image_page.get_attribute('src')
-            if image_url:
-                try:
-                    # Download the image using requests%
-                    response = requests.get(image_url)
-                    response.raise_for_status()
 
-                    image = Image.open(BytesIO(response.content))
-                    image.save(savingPath_with_file)
-                except Exception as e:
-                    screenshot = self.driver.find_element(By.CLASS_NAME, 'shrinkToFit')
-                    screenshot.screenshot(savingPath_with_file)
-            else:
-                return False
-            return
-        except:
-            try:
-                image_page = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
-                image_url = image_page.get_attribute('src')
-                if image_url:
-                    screenshot = self.driver.find_element(By.TAG_NAME, 'img')
-                    screenshot.screenshot(savingPath_with_file)
-                else:
-                    return False
-                return
-            except:
-                return False
-        finally:
+        try:
+            image_page = WebDriverWait(self.driver,10).until(presence_of_element_with_img_alt((By.TAG_NAME, 'img'),url))
+            image_page.screenshot(savingPath_with_file)
             return True
+        except TimeoutException:
+            print("image to url not found ", url)
+            return False
 
     def Quit(self):
         self.driver.close()
